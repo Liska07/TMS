1a27b521359cc8a2517e56c80a93f42209855034
Liska <07liska@gmail.com>
2023-12-18 10:46:12 -0500

    add HW_1

--- TMS/Program.cs
+++ TMS/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string userName;
+Console.Write("Введите Ваше имя: ");
+userName = Console.ReadLine();
+Console.WriteLine($"Hello, {userName}");
