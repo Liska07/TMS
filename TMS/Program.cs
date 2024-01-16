196144dcc78c490769d79c87d74750562cb0e8f8
Liska <07liska@gmail.com>
2024-01-15 20:59:50 -0500

    Add HW_6 Task1

--- TMS/Program.cs
+++ TMS/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TMS
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_1();
+
+            static void Task_1()
+            {
+                Square s1Green = new Square(5, "Green");
+                s1Green.ShowInfo("зелёного");
+
+                Square s2Blue = new Square(2, "Blue");
+                s2Blue.ShowInfo("синего");
+
+                Square s3LightBlue = new Square(15, "LightBlue");
+                s3LightBlue.ShowInfo("голубого");
+
+                s1Green.SideLength = s2Blue.SideLength * 3;
+
+                s1Green.ShowInfo("зелёного");
+                s2Blue.ShowInfo("синего");
+                s3LightBlue.ShowInfo("голубого");
+            }
+        }
+    }
+}
---
+++ TMS/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS
+{
+    public class Square
+    {
+        private int _sideLength;
+        public int SideLength 
+        {
+            get 
+            { 
+                return _sideLength; 
+            }
+            set 
+            {
+                _sideLength = value;
+                areaOfSquare = value * value;
+            } 
+        }
+        private int areaOfSquare;
+
+        public string Color {  get; set; }
+
+        public Square(int sideLength, string color) 
+        {
+            SideLength = sideLength;
+            Color = color;
+        }
+
+        public void ShowInfo(string nameSquare)
+        {
+            Console.WriteLine($"\nЦвет {nameSquare} квадрата: {Color}");
+            Console.WriteLine($"Длинна стороны {nameSquare} квадрата = {SideLength}");
+            Console.WriteLine($"Площадь {nameSquare} квадрата = {areaOfSquare}");
+            
+        }
+    }
+}
+
